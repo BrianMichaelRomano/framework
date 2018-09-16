cc8952fa10b020480b822c22bd7b12451e52e014
Brian <brianmromano@gmail.com>
2018-09-16 11:04:34 -0400

    removed console.log

--- utils/Proxify.js
+++ utils/Proxify.js
@@ -3,7 +3,6 @@ export default function(target) {
     set(target, prop, value) {
       const oldValue = target[prop];
       target[prop] = value;
-      console.log(`${prop} changed from ${oldValue} to ${target[prop]}`);
       target.router.route();
       return true;
     }
